6b8ccecfc676b4e841a764b2729c83b98a78d543
matheuscmorais356 <matheuscmorais356@gmail.com>
2023-11-13 12:22:44 -0300

    as-200 - feat: Criando modal que mostra as musicas do àlbum

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,31 @@
 import "./Card.css";
 
-const Card = ({image, name}) => {
+import { useContext } from "react";
+
+import { ModalContext } from "../../context/ModalContext";
+import { LoadingContext } from "../../context/LoadingContext";
+
+const Card = ({image, name, albumId = null}) => {
+  const { setModalInfo } = useContext(ModalContext);
+  const { setLoadingModal } = useContext(LoadingContext);
+
+  const showModal = () => {
+    setLoadingModal(true);
+
+    const albumInfo = {
+      idAlbum: albumId,
+      nameAlbum: name,
+      imageAlbum: image
+    };
+
+    setModalInfo(albumInfo);
+
+    const modal = document.querySelector("#modal");
+    modal.classList.remove("hide");
+  }
+
   return (
-    <div className="card">
+    <div className="card" onClick={albumId ? showModal : () => {}}>
       <img src={image} alt={`Foto de capa da música ${name}`} />
       <p>{name}</p>
     </div>
